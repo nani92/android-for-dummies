9be0785d07a4eb085a9759432e4c6977f7a3c04c
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-29 15:25:04 +0100

    initial

---
+++ app/src/main/java/eu/napcode/android_for_dummies/MainActivity.kt
@@ -0,0 +1,12 @@
+package eu.napcode.android_for_dummies
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
