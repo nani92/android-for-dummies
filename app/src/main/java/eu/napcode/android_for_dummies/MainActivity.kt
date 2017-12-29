a67cb1c75003919b98efdc29260bbaa7737af17b
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-29 15:32:38 +0100

    setting up rv

--- app/src/main/java/eu/napcode/android_for_dummies/MainActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/MainActivity.kt
@@ -2,6 +2,8 @@ package eu.napcode.android_for_dummies
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupRecyclerView()
+    }
 
+    private fun setupRecyclerView() {
+        recyclerView.layoutManager = getLayoutManager()
+    }
+
+    private fun getLayoutManager() : LinearLayoutManager {
+        var layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        return layoutManager
     }
 }
