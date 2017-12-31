bd989f4938c1c6f6fa2120d26780e159a95e0b5b
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-31 12:29:27 +0100

    created guide activity

---
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageGuideActivity.kt
@@ -0,0 +1,13 @@
+package eu.napcode.android_for_dummies.sendImage
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import eu.napcode.android_for_dummies.R
+
+class SendImageGuideActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_image_guide)
+    }
+}
