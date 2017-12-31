60d2f5cc4d80f956f33475dafecc8a13888469fc
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-31 12:32:08 +0100

    start activity on button clik)

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageMainActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageMainActivity.kt
@@ -1,14 +1,17 @@
 package eu.napcode.android_for_dummies.sendImage
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import eu.napcode.android_for_dummies.R
+import kotlinx.android.synthetic.main.activity_send_image_main.*
 
 class SendImageMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_send_image_main)
-    }
 
+        showHowToSendImageButton.setOnClickListener { startActivity(Intent(this, SendImageGuideActivity::class.java)) }
+    }
 }
