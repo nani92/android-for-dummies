92b08f940f12e23c689fdbd2119287296794fe81
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-31 12:44:14 +0100

    implementing stepper listener

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageGuideActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageGuideActivity.kt
@@ -2,9 +2,27 @@ package eu.napcode.android_for_dummies.sendImage
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.stepstone.stepper.StepperLayout
+import com.stepstone.stepper.VerificationError
 import eu.napcode.android_for_dummies.R
 
-class SendImageGuideActivity : AppCompatActivity() {
+class SendImageGuideActivity : AppCompatActivity(), StepperLayout.StepperListener {
+    override fun onStepSelected(newStepPosition: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onError(verificationError: VerificationError?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onReturn() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCompleted(completeButton: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
