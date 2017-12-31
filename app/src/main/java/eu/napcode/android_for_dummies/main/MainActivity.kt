b28485dbc60254d46f10ba0559290563f412018b
Natalia Jastrzębska <nataliak@10g.pl>
2017-12-31 12:13:56 +0100

    added button for send image main screen

--- app/src/main/java/eu/napcode/android_for_dummies/main/MainActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/main/MainActivity.kt
@@ -13,8 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MainActivity::class.java
-
         setupRecyclerView()
     }
 
--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageMainActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/SendImageMainActivity.kt
@@ -1,7 +1,14 @@
 package eu.napcode.android_for_dummies.sendImage
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import eu.napcode.android_for_dummies.R
 
 class SendImageMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_image_main)
+    }
 
 }
