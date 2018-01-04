a76f36997b0e7317818be34123d7145229d4c271
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-04 10:14:33 +0100

    tries to open menu not success

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/SendImageGuideActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/SendImageGuideActivity.kt
@@ -2,6 +2,8 @@ package eu.napcode.android_for_dummies.sendImage.guide
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import eu.napcode.android_for_dummies.R
 import kotlinx.android.synthetic.main.activity_send_image_guide.*
 
@@ -14,7 +16,36 @@ class SendImageGuideActivity : AppCompatActivity() {
         stepperLayout.adapter = SendImageGuideStepperAdapter(supportFragmentManager, this)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.image_preview_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        super.onPrepareOptionsMenu(menu)
+        openOptionsMenu()
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return false
+    }
+
     fun displayTitle(titleId: Int) {
         supportActionBar!!.title = getString(titleId)
+        openOptionsMenu()
+        openOptionsMenu()
+    }
+
+    fun showMenu() {
+
+        openOptionsMenu()
+        //if (supportActionBar != null)
+        //supportActionBar!!.openOptionsMenu()
+    }
+
+    override fun onMenuOpened(featureId: Int, menu: Menu?): Boolean {
+        return super.onMenuOpened(featureId, menu)
     }
 }
--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/ShareFromImagePreviewFragment.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/ShareFromImagePreviewFragment.kt
@@ -1,9 +1,11 @@
 package eu.napcode.android_for_dummies.sendImage.guide
 
+import android.app.ActionBar
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.view.*
 import android.widget.Toast
 import com.stepstone.stepper.Step
@@ -12,13 +14,22 @@ import eu.napcode.android_for_dummies.R
 import eu.napcode.android_for_dummies.base.OverlayActivity
 import eu.napcode.android_for_dummies.base.SHOW_VIEW_RIGHT_CORNER
 
-var OVERLAY_ACTIVITY_REQUEST_CODE = 100
+var OVERLAY_ACTIVITY_REQUEST_CODE_SHARE = 101
+var OVERLAY_ACTIVITY_REQUEST_CODE_SETTINGS = 102
 
 class ShareFromImagePreviewFragment : Fragment(), Step {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
+
+       // setHasOptionsMenu(true)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+      //  setMenuVisibility(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,7 +37,12 @@ class ShareFromImagePreviewFragment : Fragment(), Step {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater!!.inflate(R.menu.image_preview_menu, menu)
+        //inflater!!.inflate(R.menu.image_preview_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?) {
+        super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -40,10 +56,14 @@ class ShareFromImagePreviewFragment : Fragment(), Step {
     override fun onSelected() {
         (activity as SendImageGuideActivity).displayTitle(R.string.gallery)
 
+        showRightCorner(OVERLAY_ACTIVITY_REQUEST_CODE_SHARE)
+    }
+
+    fun showRightCorner(requestCode: Int) {
         var intent = Intent(context, OverlayActivity::class.java)
         intent.putExtra(SHOW_VIEW_RIGHT_CORNER, true)
 
-        startActivityForResult(intent, OVERLAY_ACTIVITY_REQUEST_CODE)
+        startActivityForResult(intent, requestCode)
     }
 
     override fun verifyStep(): VerificationError? {
@@ -57,9 +77,17 @@ class ShareFromImagePreviewFragment : Fragment(), Step {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == OVERLAY_ACTIVITY_REQUEST_CODE
+        if (requestCode == OVERLAY_ACTIVITY_REQUEST_CODE_SHARE
                 && resultCode == Activity.RESULT_OK) {
             Toast.makeText(context, "dupa", Toast.LENGTH_LONG).show()
+
+            if ((activity as AppCompatActivity)!!.supportActionBar != null) {
+
+                (activity as SendImageGuideActivity).openOptionsMenu()
+               // (activity as SendImageGuideActivity).invalidateOptionsMenu()
+            }
+
+            //showRightCorner(OVERLAY_ACTIVITY_REQUEST_CODE_SETTINGS)
         }
     }
 }
