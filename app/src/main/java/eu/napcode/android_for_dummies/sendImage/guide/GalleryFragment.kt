af8c6fb1f9eb22d26558dbbf778f06b7033120fd
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-08 11:17:56 +0100

    added gallery fragment

---
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/GalleryFragment.kt
@@ -0,0 +1,22 @@
+package eu.napcode.android_for_dummies.sendImage.guide
+
+import android.app.Fragment
+import com.stepstone.stepper.Step
+import com.stepstone.stepper.VerificationError
+
+
+class GalleryFragment : Fragment(), Step {
+
+    override fun verifyStep(): VerificationError? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onError(error: VerificationError) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSelected() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
