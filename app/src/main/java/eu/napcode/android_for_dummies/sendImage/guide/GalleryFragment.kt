82b575c13852d51d35a1dad2e67d03cee69250de
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-08 11:19:28 +0100

    added layout for galler frag

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/GalleryFragment.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/GalleryFragment.kt
@@ -1,11 +1,20 @@
 package eu.napcode.android_for_dummies.sendImage.guide
 
 import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.stepstone.stepper.Step
 import com.stepstone.stepper.VerificationError
+import eu.napcode.android_for_dummies.R
 
 
 class GalleryFragment : Fragment(), Step {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_send_image_guide_gallery, container, false)
+    }
 
     override fun verifyStep(): VerificationError? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
