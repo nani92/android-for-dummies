731bf86a9dcb7193730e8536f61611d9f78d3bfd
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-09 22:46:08 +0100

    added layout for mentioned above activity

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/help/HelpSendImageActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/help/HelpSendImageActivity.kt
@@ -1,7 +1,13 @@
 package eu.napcode.android_for_dummies.sendImage.help
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import eu.napcode.android_for_dummies.R
 
 class HelpSendImageActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help_send_image)
+    }
 }
