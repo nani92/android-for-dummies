e4dc0f302de3fb6ffd74da7931e82019f915b077
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-16 14:34:44 +0100

    fixed hole for bottom nav

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/ShareFromImagePreviewFragment2.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/ShareFromImagePreviewFragment2.kt
@@ -3,6 +3,7 @@ package eu.napcode.android_for_dummies.sendImage.guide
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationMenuView
 import android.support.v4.app.Fragment
 import android.view.*
 import com.stepstone.stepper.Step
@@ -27,10 +28,11 @@ class ShareFromImagePreviewFragment2 : Fragment(), Step {
     }
 
     fun showView(text: String) {
+        var shareButton = (bottomNavigationView.getChildAt(0) as BottomNavigationMenuView).getChildAt(0)
         var intent = Intent(context, OverlayActivity::class.java)
-        intent.putExtra(SHOW_VIEW_ELEMENT_LEFT_KEY, bottomNavigationView.getChildAt(0).left)
+        intent.putExtra(SHOW_VIEW_ELEMENT_LEFT_KEY, shareButton.left)
         intent.putExtra(SHOW_VIEW_ELEMENT_TOP_KEY, bottomNavigationView.top)
-        intent.putExtra(SHOW_VIEW_ELEMENT_RIGHT_KEY, bottomNavigationView.getChildAt(0).right)
+        intent.putExtra(SHOW_VIEW_ELEMENT_RIGHT_KEY, shareButton.right)
         intent.putExtra(SHOW_VIEW_ELEMENT_BOTTOM_KEY, bottomNavigationView.bottom)
         intent.putExtra(DISPLAY_TEXT_VALUE_KEY, text)
         intent.putExtra(DISPLAY_TEXT_PLACE_KEY, DISPLAY_TEXT_TOP)
