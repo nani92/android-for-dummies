694c7de8dd1ada48605fbc0d854919d656e59807
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-17 12:03:24 +0100

    created layout for bottom sheet frag

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareBottomSheetFragment.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareBottomSheetFragment.kt
@@ -1,12 +1,20 @@
 package eu.napcode.android_for_dummies.sendImage.guide.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import eu.napcode.android_for_dummies.R
 import eu.napcode.android_for_dummies.base.BaseFragmentStep
 import eu.napcode.android_for_dummies.sendImage.guide.SendImageGuideActivity
 
 class ShareBottomSheetFragment : BaseFragmentStep() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_share_bottom_sheet, container, false)
+    }
+
     override fun onSelected() {
         (activity as SendImageGuideActivity).displayedAllGuide = true
-
     }
 }
