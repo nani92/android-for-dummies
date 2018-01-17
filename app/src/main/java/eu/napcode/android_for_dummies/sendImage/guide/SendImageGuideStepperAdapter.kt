f91dc532a6536f794ccd23777bdcd91c37a3c16f
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-17 12:00:39 +0100

    added new step

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/SendImageGuideStepperAdapter.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/SendImageGuideStepperAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager
 import com.stepstone.stepper.Step
 import com.stepstone.stepper.adapter.AbstractFragmentStepAdapter
 import eu.napcode.android_for_dummies.sendImage.guide.fragments.GalleryFragment
+import eu.napcode.android_for_dummies.sendImage.guide.fragments.ShareBottomSheetFragment
 import eu.napcode.android_for_dummies.sendImage.guide.fragments.ShareFromImagePreviewFragment
 import eu.napcode.android_for_dummies.sendImage.guide.fragments.ShareFromImagePreviewFragment2
 
@@ -12,7 +13,8 @@ class SendImageGuideStepperAdapter(fm: FragmentManager, context: Context) : Abst
 
     var steps : Array<Step> = arrayOf(GalleryFragment(),
             ShareFromImagePreviewFragment(),
-            ShareFromImagePreviewFragment2())
+            ShareFromImagePreviewFragment2(),
+            ShareBottomSheetFragment())
 
     override fun getCount(): Int = steps.count()
 
---
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareBottomSheetFragment.kt
@@ -0,0 +1,7 @@
+package eu.napcode.android_for_dummies.sendImage.guide.fragments
+
+import eu.napcode.android_for_dummies.base.BaseFragmentStep
+
+class ShareBottomSheetFragment : BaseFragmentStep() {
+
+}
