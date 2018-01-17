afe65993c17acf7371651620c20d2a2178e97191
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-17 12:02:19 +0100

    proceed to next step

--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareBottomSheetFragment.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareBottomSheetFragment.kt
@@ -1,7 +1,12 @@
 package eu.napcode.android_for_dummies.sendImage.guide.fragments
 
 import eu.napcode.android_for_dummies.base.BaseFragmentStep
+import eu.napcode.android_for_dummies.sendImage.guide.SendImageGuideActivity
 
 class ShareBottomSheetFragment : BaseFragmentStep() {
 
+    override fun onSelected() {
+        (activity as SendImageGuideActivity).displayedAllGuide = true
+
+    }
 }
--- app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareFromImagePreviewFragment2.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/sendImage/guide/fragments/ShareFromImagePreviewFragment2.kt
@@ -4,10 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.internal.BottomNavigationMenuView
-import android.support.v4.app.Fragment
 import android.view.*
-import com.stepstone.stepper.Step
-import com.stepstone.stepper.VerificationError
 import eu.napcode.android_for_dummies.R
 import eu.napcode.android_for_dummies.base.*
 import eu.napcode.android_for_dummies.sendImage.guide.SendImageGuideActivity
@@ -15,14 +12,13 @@ import kotlinx.android.synthetic.main.fragment_share_from_image_preview2.*
 
 var OVERLAY_ACTIVITY_REQUEST_CODE_BOTTOM_SHARE = 104
 
-class ShareFromImagePreviewFragment2 : Fragment(), Step {
+class ShareFromImagePreviewFragment2 : BaseFragmentStep() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_share_from_image_preview2, container, false)
     }
 
     override fun onSelected() {
-        (activity as SendImageGuideActivity).displayedAllGuide = true
         (activity as SendImageGuideActivity).hideActions()
 
         showView(getString(R.string.share_tip2))
@@ -41,10 +37,6 @@ class ShareFromImagePreviewFragment2 : Fragment(), Step {
         startActivityForResult(intent, OVERLAY_ACTIVITY_REQUEST_CODE_BOTTOM_SHARE)
     }
 
-    override fun verifyStep(): VerificationError? = null
-
-    override fun onError(error: VerificationError) {}
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -57,7 +49,7 @@ class ShareFromImagePreviewFragment2 : Fragment(), Step {
         when (requestCode) {
 
             OVERLAY_ACTIVITY_REQUEST_CODE_SHARE -> {
-                //onP
+                onProceedListener.onProceed()
             }
         }
     }
