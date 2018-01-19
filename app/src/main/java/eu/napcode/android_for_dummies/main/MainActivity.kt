942e321b9fc768d764e532e15263b37c17642033
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-19 16:47:05 +0100

    fixed trans names

--- app/src/main/java/eu/napcode/android_for_dummies/main/MainActivity.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/main/MainActivity.kt
@@ -8,8 +8,6 @@ import android.support.v7.widget.LinearLayoutManager
 import eu.napcode.android_for_dummies.R
 import eu.napcode.android_for_dummies.base.getHelpEntries
 import kotlinx.android.synthetic.main.activity_main.*
-import android.R.attr.transitionName
-import android.app.Activity
 import android.support.v7.widget.AppCompatImageView
 import android.util.Pair
 import android.view.View
@@ -33,8 +31,8 @@ class MainActivity : AppCompatActivity() {
             override fun onItemClick(helpEntry: HelpEntry, view: View) {
                 val transitionActivityOptions = ActivityOptions
                         .makeSceneTransitionAnimation(this@MainActivity,
-                                Pair<View, String>(view.findViewById<AppCompatImageView>(R.id.help_image_view), getString(R.string.guide_image_view_transition_name)),
-                                Pair<View, String>(view.findViewById<TextView>(R.id.help_name_text_view), getString(R.string.guide_title_text_view_transition_name)))
+                                Pair<View, String>(view.findViewById<AppCompatImageView>(R.id.help_image_view), getString(R.string.help_entry_image_view_transition_name)),
+                                Pair<View, String>(view.findViewById<TextView>(R.id.help_name_text_view), getString(R.string.help_entry_title_text_view_transition_name)))
                 startActivity(helpEntry.activityIntent, transitionActivityOptions.toBundle())
             }
         })
