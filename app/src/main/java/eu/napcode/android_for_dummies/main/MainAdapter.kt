f1706fd2c8eba38dc03b248816e1a90e5e7e03c4
Natalia Jastrzębska <nataliak@10g.pl>
2018-01-31 10:15:55 +0100

    fixes

--- app/src/main/java/eu/napcode/android_for_dummies/main/MainAdapter.kt
+++ app/src/main/java/eu/napcode/android_for_dummies/main/MainAdapter.kt
@@ -2,11 +2,11 @@ package eu.napcode.android_for_dummies.main
 
 import android.content.Context
 import android.support.annotation.LayoutRes
+import android.support.v7.widget.AppCompatImageView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import eu.napcode.android_for_dummies.base.HelpEntry
 import eu.napcode.android_for_dummies.R
@@ -26,7 +26,7 @@ class MainAdapter(var context: Context, var helpEntries: ArrayList<HelpEntry>, v
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(helpEntry: HelpEntry, listener: OnItemClickListener) {
             itemView.findViewById<TextView>(R.id.help_name_text_view).setText(helpEntry.nameResourceId)
-            itemView.findViewById<ImageView>(R.id.help_image_view).setImageDrawable(context.getDrawable(helpEntry.imageResourceId))
+            itemView.findViewById<AppCompatImageView>(R.id.help_image_view).setImageDrawable(context.getDrawable(helpEntry.imageResourceId))
 
             itemView.setOnClickListener { listener.onItemClick(helpEntry, itemView) }
         }
