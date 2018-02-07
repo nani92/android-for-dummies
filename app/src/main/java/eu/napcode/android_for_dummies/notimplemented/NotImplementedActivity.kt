e7d6b5cc7044cc41af444354e51fa02339485102
Natalia Jastrzębska <nataliak@10g.pl>
2018-02-07 19:05:57 +0100

    added not implemented activity

---
+++ app/src/main/java/eu/napcode/android_for_dummies/notimplemented/NotImplementedActivity.kt
@@ -0,0 +1,14 @@
+package eu.napcode.android_for_dummies.notimplemented
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import eu.napcode.android_for_dummies.R
+
+class NotImplementedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_not_implemented)
+    }
+}
